3e86fc31b652b8e49fc3c86b241e4b922e5940b8
dakshjun1417 <74766800+dakshjun1417@users.noreply.github.com>
2020-11-30 20:17:48 +0530

    Add files via upload

---
+++ caesar.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(int argc, string argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage: ./caesar key");
+        return 1;
+    }
+    {
+        
+    }
+    int a = atoi(argv[1]);
+    int a1 = a%26;
+
+        string s = get_string("plaintext: ");
+        printf("ciphertext: ");
+        for(int i = 0; i < strlen(s); i++)
+        {
+        if(s[i] >= 65 && s[i] <= 90)
+        {
+            if(s[i] + a1 <= 90)
+            {
+                printf("%c", s[i] + a1);
+            }
+            else if(s[i] + a1 > 90)
+            {
+                printf("%c", s[i] + a1 - 26);
+            }
+        }
+        else if(s[i] >= 97 && s[i] <= 122)
+        {
+            if(s[i] + a1 <= 122)
+            {
+                printf("%c", s[i] + a1);
+            }
+            else if(s[i] + a1 > 122)
+            {
+                printf("%c", s[i] + a1 - 26);
+            }
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+
+}
